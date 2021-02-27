64302d5073e12ab70a20f800664b88415a43d905
Zoriana Chyzhovska <zoriana.chyzhovska@gmail.com>
2021-02-27 17:23:04 +0200

    Menu
    added menu header, menu, icons

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -1,26 +1,29 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
-import java.net.URL;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private FirebaseAuth mAuth;
 
     private ImageView profileImage;
@@ -28,6 +31,13 @@ public class DashboardActivity extends AppCompatActivity {
     private TextView nameTxt;
     private TextView emailTxt;
     private Button btnSignOut;
+
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,35 +48,83 @@ public class DashboardActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         showUserData(currentUser);
-        handleLogOut();
+
+        //------------------Hooks------------------
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //------------------Tool Bar------------------
+        setSupportActionBar(toolbar);
+        // ------------------Nav drawer menu------------------
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
     }
 
     private void showUserData(FirebaseUser currentUser){
-        nameTxt = (TextView) findViewById(R.id.name_txt);
+        navigationView = findViewById(R.id.nav_view);
+
+        nameTxt = (TextView)navigationView.getHeaderView(0).findViewById(R.id.name_txt);
         nameTxt.setText(currentUser.getDisplayName());
 
-        emailTxt = (TextView) findViewById(R.id.email_txt);
+        emailTxt = (TextView)navigationView.getHeaderView(0).findViewById(R.id.email_txt);
         emailTxt.setText(currentUser.getEmail());
 
-        profileImage = (ImageView) findViewById(R.id.profile_image);
+        profileImage = (ImageView)navigationView.getHeaderView(0).findViewById(R.id.profile_image);
         Picasso.get().load(currentUser.getPhotoUrl()).into(profileImage);
     }
 
-    private void handleLogOut(){
 
-        btnSignOut = (Button) findViewById(R.id.sign_out_btn);
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Intent intent;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_words:
+                Toast.makeText(this, "words", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_rules:
+                Toast.makeText(this, "rules", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_useful:
+                Toast.makeText(this, "useful", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
                 FirebaseAuth.getInstance().signOut();
 
                 GoogleSignIn.getClient(getBaseContext(),
                         new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
                 ).signOut();
 
-                Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+                intent = new Intent(DashboardActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
     }
 }
+
